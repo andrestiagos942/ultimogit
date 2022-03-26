0dd91bf248b143a60f7e6f4ffb3e98edcf8c5920
unknown <andrestiagos942@gmail.com>
2022-03-26 09:51:07 -0300

    se vio operadores basicos

--- JavaScriptDW/ComandosBasicos.js
+++ JavaScriptDW/ComandosBasicos.js
@@ -1,5 +1,6 @@
-//----- COMANDOS BASICOS
+//----- COMANDOS BASICOS por consola de navegador
 
+// CLASE 41 JS
 // Ejemplo 1
 var numero = 20
 var numero2 = 10
@@ -18,3 +19,19 @@ console.log(resultadoej2)
 
 var nombre = prompt("escribi tu nombre")
 nombre 
+
+
+//  CLASE 43 - operadores 
+// Comparaciones "true y false"
+
+ 1 == 1 true
+ 1 == 2 false
+ 1 > 4 False
+ 1 < 4 true 
+ 4 >= 4 true
+ 1 != 1 False   distinto
+ 1 != 2 true    distinto 
+
+ 
+
+
