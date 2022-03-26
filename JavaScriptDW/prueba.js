c7c2a4d7d0665300b214aaaa7688c55dcb5cdd4d
unknown <andrestiagos942@gmail.com>
2022-03-26 09:06:33 -0300

    comandos basicos en js, algunos ejemplos

---
+++ JavaScriptDW/prueba.js
@@ -0,0 +1,20 @@
+//----- COMANDOS BASICOS
+
+// Ejemplo 1
+var numero = 20
+var numero2 = 10
+var resultadoej1 = numero + numero2
+console.log(resultadoej1)
+
+// Ejemplo 2 
+
+var cadena1 = "hola"
+var cadena2 = "jau ar shu ?"
+var separador = " "
+var resultadoej2= cadena1 + separador + cadena2 
+console.log(resultadoej2)
+
+// Ejemplo 3 guarda variable
+
+var nombre = prompt("escribi tu nombre")
+nombre 
