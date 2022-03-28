e17f015147cb8dc93635ec15973f2ac85a7cfe0e
unknown <andrestiagos942@gmail.com>
2022-03-27 23:11:03 -0300

    primer bucle while

---
+++ JavaScriptDW/prueba3.js
@@ -0,0 +1,10 @@
+// clase 45 bucles while 
+
+var valor = 0;
+
+while (valor < 4){
+    // codigo que se ejecuta de forma repetitiva hasta que ya no se cumpla la condicion 
+    console.log("Valor es igual a " + valor);  
+    console.log("valor es menor que 4, entonces le sumo 1 a la variable valor");
+    valor = valor + 1;
+}
