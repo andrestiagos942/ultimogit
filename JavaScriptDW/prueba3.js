733aa4cdc7adac64027a346ffe1796675701ac2f
unknown <andrestiagos942@gmail.com>
2022-03-27 23:17:33 -0300

    agregado de un if para identiicar que en 2 pare con el break

--- JavaScriptDW/prueba3.js
+++ JavaScriptDW/prueba3.js
@@ -6,5 +6,10 @@ while (valor < 4){
     // codigo que se ejecuta de forma repetitiva hasta que ya no se cumpla la condicion 
     console.log("Valor es igual a " + valor);  
     console.log("valor es menor que 4, entonces le sumo 1 a la variable valor");
+ 
+    if (valor == 2){
+        console.log("ahoravalor es igual a 2 y quiero parar la ejecucion del bucle");
+        break;
+    }
     valor = valor + 1;
 }
