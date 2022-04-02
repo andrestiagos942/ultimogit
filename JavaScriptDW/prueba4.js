622671362b62c445ccbf2c4c7fda146d2d55102e
unknown <andrestiagos942@gmail.com>
2022-04-02 20:21:12 -0300

    prueba 5 agregar archivo y cambio de src

---
+++ JavaScriptDW/prueba4.js
@@ -0,0 +1,11 @@
+// video bucle for 
+
+// for (var valor = 0; valor <= 4; valor++) {
+ //   console.log("valor es igual a " + valor );
+ // }
+
+    var nombre = "andres";
+
+  for (var i = 0; i < nombre.length; i++) {
+    console.log(nombre[i])
+  }
