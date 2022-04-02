677707d116beff851df5d5a943be68b129053693
unknown <andrestiagos942@gmail.com>
2022-04-02 20:44:40 -0300

    funciones js

---
+++ JavaScriptDW/prueba5.js
@@ -0,0 +1,11 @@
+// java script funciones 
+
+function hola(nombre, edad) {
+    var frase = "hola" + nombre + "que ondinha" + " feliz " + edad + " cumpleañito ";
+    return frase;
+}
+
+var cadena = hola(" pepe ", 22);
+console.log(cadena);
+var cadena = hola(" pepito ", 15);
+console.log(cadena);
