279102239300522565ff08cf8537c422d1fa56a2
unknown <andrestiagos942@gmail.com>
2022-04-02 20:51:35 -0300

    ambito de variables v48

--- JavaScriptDW/prueba5.js
+++ JavaScriptDW/prueba5.js
@@ -1,10 +1,13 @@
-// java script funciones 
+// java script funcionesv47 + ambito de funciones V48
 
+//v48 variable global
+var mensaje1 = "pasalo peola breoh";
+
+// de aca para abajo es v47
 function hola(nombre, edad) {
-    var frase = "hola" + nombre + "que ondinha" + " feliz " + edad + " cumpleañito ";
+    var frase = "hola" + nombre + "que ondinha" + " feliz " + edad + " cumpleañito " + mensaje1;
     return frase;
 }
-
 var cadena = hola(" pepe ", 22);
 console.log(cadena);
 var cadena = hola(" pepito ", 15);
