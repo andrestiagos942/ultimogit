6c23791e7d5bc2a094044fdf93d4a8c4f23695bf
unknown <andrestiagos942@gmail.com>
2022-04-04 00:30:24 -0300

    arrays

--- JavaScriptDW/ComandosBasicos.js
+++ JavaScriptDW/ComandosBasicos.js
@@ -34,6 +34,36 @@ nombre
 
 
  
- 
+ //  CLASE 49 - Arrays arraigadas a v48 y v48 
 
+// Array: estructura donde se pueden almacenar diferentes valores
 
+// array de nombres
+ var nombres = ["antonio","Pablo","Maria","Luis"]
+    //llamado del nombre por valor de la variable
+    nombres [2]
+    'Maria'
+     nombres [3]
+    'Luis'
+    //longitud del array
+    nombres.length
+    // borrar ulitmo valor
+    nombres.pop()
+    // agregar valor
+    nombres.push("juan")
+    // variable array matriz
+    var matriz = [ [1,2,3], [6,7,8] ]
+    // bucle for para var nombre listado en numero
+    for (var i = 0; i<nombres.length; i++){
+        console.log(nombres[i]);
+    }
+    // bucle for para var nombre listado por nombres
+    for (var i = 0; i<nombres.length; i++){
+        console.log(nombres[i]);
+    }   
+    // bucle for para var nombre listado por nombres
+    for (nombre of nombres){
+        console.log(nombre);
+    }
+    // for each con alert
+    nombres.forEach(alert)
