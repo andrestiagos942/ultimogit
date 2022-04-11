40c7e2c76ad5675ce622e87d54e1455fe2d49b60
unknown <andrestiagos942@gmail.com>
2022-04-11 01:54:09 -0300

    agregar comentario

--- JavaScriptDW/Ejercicio2/ejercicio.js
+++ JavaScriptDW/Ejercicio2/ejercicio.js
@@ -6,9 +6,11 @@ var maxnum = parseInt(dato1);
 
 var numero = 0;
 var texto = " ";
+// maxnum se le suma uno, para que incluya el ingresado por usuario
 while (numero < (maxnum + 1)){
+    // es un numero par
     if (numero % 2 == 0){
-        // es un numero par
+        
         texto = texto + numero + " ";
     }
     numero = numero + 1;
