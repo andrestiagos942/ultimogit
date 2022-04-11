ff5e187940da486adb051131730b1e74664b777e
unknown <andrestiagos942@gmail.com>
2022-04-11 03:48:22 -0300

    ejercicio 5

---
+++ JavaScriptDW/ejercicios/Ejercicio5/ejercicio.js
@@ -0,0 +1,13 @@
+
+
+
+
+var persona = {
+ nombre:"andres",
+  apellido: "sotovich",
+  edad: 35,  
+  info : function(){
+      alert("Mi nombre es" + nombre + " " + apellido + " mi edad es " + edad);
+  }
+}
+console.log(persona)
