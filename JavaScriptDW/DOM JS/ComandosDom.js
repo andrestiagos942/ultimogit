016af281b9733aaa1fd1257d36605549fdb55abe
unknown <andrestiagos942@gmail.com>
2022-04-17 05:27:07 -0300

    Contenido comandos

---
+++ JavaScriptDW/DOM JS/ComandosDom.js
@@ -0,0 +1,9 @@
+// selector de elementos ejemplo h1, ahora variable titular es el h1
+var titular = document.querySelector("h1") 
+
+// cambiarle color a la variable titular 
+titular.style.color = "green"
+// mismo ejemplo que arriba 
+var parrafo = document.querySelector("p")
+parrafo.style = "red"
+parrafo.style.fontSize = "14px"
