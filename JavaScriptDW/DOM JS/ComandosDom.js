168e8bb59698c0ae143b3ef9629623f6327cb8a0
unknown <andrestiagos942@gmail.com>
2022-04-17 05:33:35 -0300

    Dom contenido y html

--- JavaScriptDW/DOM JS/ComandosDom.js
+++ JavaScriptDW/DOM JS/ComandosDom.js
@@ -1,3 +1,4 @@
+// Clase 65
 // selector de elementos ejemplo h1, ahora variable titular es el h1
 var titular = document.querySelector("h1") 
 
@@ -7,3 +8,12 @@ titular.style.color = "green"
 var parrafo = document.querySelector("p")
 parrafo.style = "red"
 parrafo.style.fontSize = "14px"
+
+
+
+// Clase 66
+// Editar texto en parrafo 
+parrafo.textContent = "esto es un texto nuevo para este parrafo";
+'esto es un texto nuevo para este parrafo'
+// Editar Html en un parrafo
+parrafo.innerHTML = "esto es un parrafo con algo en <b>negrita<b>";
